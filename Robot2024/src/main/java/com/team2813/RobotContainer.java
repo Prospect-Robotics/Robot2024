328c35345353f49125ccdc5dc67b5b5031109b74
cuttestkittensrule <mangoiscute95@gmail.com>
2024-01-13 16:09:34 -0800

    added Drive to RobotContainer

--- Robot2024/src/main/java/com/team2813/RobotContainer.java
+++ Robot2024/src/main/java/com/team2813/RobotContainer.java
@@ -10,13 +10,19 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 // import frc.robot.Constants.OperatorConstants;
 import edu.wpi.first.wpilibj2.command.Command;
+import com.team2813.subsystems.Drive;
 
 public class RobotContainer {
 	private final SendableChooser<Command> autoChooser;
+	private final Drive drive = new Drive();
 	public RobotContainer() {
 		configureBindings();
 		autoChooser = AutoBuilder.buildAutoChooser();
 		SmartDashboard.putData("Auto", autoChooser);
+	}
+
+	public Drive getDrive() {
+		return drive;
 	}
 
 	private void configureBindings() {}
