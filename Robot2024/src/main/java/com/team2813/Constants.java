32cf33ab1308b6e48c99b62a8ede7ccff97d8cd9
Brian Mai <0hrs3g@gmail.com>
2024-01-17 20:29:36 -0800

    id

--- Robot2024/src/main/java/com/team2813/Constants.java
+++ Robot2024/src/main/java/com/team2813/Constants.java
@@ -14,6 +14,6 @@ package com.team2813;
  */
 public final class Constants {
   public static class OperatorConstants {
-    public static final int kDriverControllerPort = 0;
+    public static final int CLIMBER = 18;
   }
 }
