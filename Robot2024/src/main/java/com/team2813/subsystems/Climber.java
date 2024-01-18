cbb86b59d2f8a5b6b288376022a5837cf9c18aa3
Brian Mai <0hrs3g@gmail.com>
2024-01-17 20:29:42 -0800

    init

---
+++ Robot2024/src/main/java/com/team2813/subsystems/Climber.java
@@ -0,0 +1,13 @@
+package com.team2813.subsystems;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import static com.team2813.Constants.*;
+
+@SuppressWarnings("unused")
+public class Climber extends SubsystemBase {
+    public Climber() {
+
+    }
+}
