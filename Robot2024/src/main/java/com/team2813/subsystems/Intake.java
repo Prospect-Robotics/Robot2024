050d8b8fbc1b31c3cc1b721cddd464cadf4a7bfd
Cassidy Mark <cassidy.mark2007@gmail.com>
2024-01-17 18:32:58 -0800

    Importing some phoenix stuff

---
+++ Robot2024/src/main/java/com/team2813/subsystems/Intake.java
@@ -0,0 +1,8 @@
+package com.team2813.subsystems;
+
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
+
+import static com.team2813.Constants.*;
+public class Intake {
+}
