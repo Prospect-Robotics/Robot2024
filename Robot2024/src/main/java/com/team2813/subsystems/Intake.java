38cd88c062407f3978206c135d3915dd3e759890
Cassidy Mark <cassidy.mark2007@gmail.com>
2024-01-18 17:56:37 -0800

    More set up

--- Robot2024/src/main/java/com/team2813/subsystems/Intake.java
+++ Robot2024/src/main/java/com/team2813/subsystems/Intake.java
@@ -4,6 +4,7 @@ import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
 
 import com.team2813.lib2813.control.motors.TalonFXWrapper;
+import com.team2813.lib2813.control.ControlMode;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -11,6 +12,10 @@ import static com.team2813.Constants.*;
 public class Intake extends SubsystemBase {
     private final TalonFXWrapper intakeMotor = new TalonFXWrapper(INTAKE, TalonFXInvertType.Clockwise);
     private final TalonFXWrapper pivotMotor = new TalonFXWrapper(INTAKE_PIVOT, TalonFXInvertType.CounterClockwise);
+
+    public Intake() {
+
+    }
 }
 
 
