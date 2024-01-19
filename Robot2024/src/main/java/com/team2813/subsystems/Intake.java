ee806d522f538bf6025827889678eaceb616d3f6
Cassidy Mark <cassidy.mark2007@gmail.com>
2024-01-18 17:37:48 -0800

    Changed version in build.gradle
    Co-Authored-By: cuttestkittensrule <mangoiscute95@gmail.com>

--- Robot2024/src/main/java/com/team2813/subsystems/Intake.java
+++ Robot2024/src/main/java/com/team2813/subsystems/Intake.java
@@ -3,6 +3,10 @@ package com.team2813.subsystems;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
 
+//import com.team2813.lib2813.control.motors;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 import static com.team2813.Constants.*;
-public class Intake {
+public class Intake extends SubsystemBase {
 }
