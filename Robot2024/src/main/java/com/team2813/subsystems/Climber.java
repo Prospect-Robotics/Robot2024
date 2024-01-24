7437efe5fd992c82123ff72326d13015173fee03
KaptainKermit <kupresaninmarko2@gmail.com>
2024-01-23 17:29:54 -0800

    add talonfx import

--- Robot2024/src/main/java/com/team2813/subsystems/Climber.java
+++ Robot2024/src/main/java/com/team2813/subsystems/Climber.java
@@ -3,6 +3,8 @@ package com.team2813.subsystems;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import com.team2813.lib2813.control.motors.TalonFXWrapper;
+
 import static com.team2813.Constants.*;
 
 @SuppressWarnings("unused")
