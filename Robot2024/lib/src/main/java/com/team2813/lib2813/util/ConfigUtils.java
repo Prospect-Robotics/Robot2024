02a9ca662ec5dc480a7b3cc5b0df883f308b5a4a
cuttestkittensrule <mangoiscute95@gmail.com>
2024-01-27 16:58:47 -0800

    added encoder code

--- Robot2024/lib/src/main/java/com/team2813/lib2813/util/ConfigUtils.java
+++ Robot2024/lib/src/main/java/com/team2813/lib2813/util/ConfigUtils.java
@@ -3,6 +3,7 @@ package com.team2813.lib2813.util;
 import java.util.function.Supplier;
 
 import com.ctre.phoenix.ErrorCode;
+import com.ctre.phoenix6.StatusCode;
 import com.revrobotics.REVLibError;
 
 import edu.wpi.first.wpilibj.DriverStation;
@@ -22,6 +23,17 @@ public class ConfigUtils {
 			errorCode = configMethod.get();
 		}
 		if (errorCode != REVLibError.kOk) {
+			DriverStation.reportError(String.format("%s: Config Failed", errorCode.toString()), false);
+		}
+	}
+
+	public static void phoenix6Config(Supplier<StatusCode> configMethod) {
+		StatusCode errorCode = configMethod.get();
+		for (int i = 1; i <= ATTEMPTS && errorCode != StatusCode.OK; i++) {
+			DriverStation.reportError(String.format("%s: Config Attempt %d Failed", errorCode.toString(), i), false);
+			errorCode = configMethod.get();
+		}
+		if (errorCode != StatusCode.OK) {
 			DriverStation.reportError(String.format("%s: Config Failed", errorCode.toString()), false);
 		}
 	}
