78e3cb1d422a43a3c5142d9d05c5e6d165cd89ec
cuttestkittensrule <mangoiscute95@gmail.com>
2024-02-01 16:12:38 -0800

    changed mag to use Motor interface and addFollower

--- Robot2024/src/main/java/com/team2813/subsystems/Magazine.java
+++ Robot2024/src/main/java/com/team2813/subsystems/Magazine.java
@@ -2,22 +2,26 @@ package com.team2813.subsystems;
 
 import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
 import com.team2813.lib2813.control.motors.TalonFXWrapper;
-//import com.team2813.lib2813.control.motors.SparkMaxWrapper;
 import com.team2813.lib2813.control.ControlMode;
+import com.team2813.lib2813.control.Motor;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import static com.team2813.Constants.*;
 public class Magazine extends SubsystemBase {
-    private final TalonFXWrapper kickerMotor = new TalonFXWrapper(KICKER, TalonFXInvertType.Clockwise);
-    private final TalonFXWrapper magazineMotor = new TalonFXWrapper(MAGAZINE, TalonFXInvertType.Clockwise);
-
-    //private final SparkMaxWrapper leftMagMotor = new SparkMaxWrapper(LEFT_MAGAZINE, CANSparkLowLevel.MotorType.kBrushless, true);
-    //private final SparkMaxWrapper rightMagMotor = new SparkMaxWrapper(RIGHT_MAGAZINE, CANSparkLowLevel.MotorType.kBrushless, true);
+    private final Motor kickerMotor;
 
     public Magazine() {
-
-
+		TalonFXWrapper kickerMotor = new TalonFXWrapper(KICKER, TalonFXInvertType.CounterClockwise);
+		kickerMotor.addFollower(MAGAZINE, TalonFXInvertType.FollowMaster);
+		this.kickerMotor = kickerMotor;
     }
 
-    public void magazine() { kickerMotor.set(ControlMode.DUTY_CYCLE, 0.5); magazineMotor.set(ControlMode.DUTY_CYCLE, 0.5); }
+    public void run() {
+		kickerMotor.set(ControlMode.DUTY_CYCLE, 0.5);
+	}
+
+	public void stop() {
+		kickerMotor.set(ControlMode.DUTY_CYCLE, 0);
+	}
 }
