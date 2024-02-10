cd2a75be50613ead046cef5273a77ec918b44fb1
cuttestkittensrule <mangoiscute95@gmail.com>
2024-02-09 17:32:03 -0800

    Gradle composite build (#17)
    * removed old lib
    
    * updated lib2813 to main
    
    * Update gradle.yml
    
    Update gradle.yml to use recursive cloning of submodules
    
    * Update gradle.yml
    
    trying suggested fix
    
    * Update gradle.yml
    
    now in the right branch :)
    also, umm, I am upgrading checkout action
    
    * updated buil.gradle and settings.gradle
    
    * updated lib
    
    * updated lib2813 to have config method
    
    * forgot very important line

--- Robot2024/lib/src/main/java/com/team2813/lib2813/control/Motor.java
+++
@@ -1,7 +0,0 @@
-package com.team2813.lib2813.control;
-
-public interface Motor {
-	// motor control
-	void set(ControlMode mode, double demand);
-	void set(ControlMode mode, double demand, double feedForward);
-}
