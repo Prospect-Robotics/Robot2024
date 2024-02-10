d1cf51a539c3477c7796b3c0812d75e8f1a13b14
Kevin Cooney <kcooney@users.noreply.github.com>
2024-02-10 08:44:00 -0800

    Fix DefaultShooterCommand (#23)
    Rename excecute() to execute() and annotate with `@Override`
    
    Also some minor cleanups:
    - Make DefaultShooterCommand final (Effective Java Item 19)
    - Make fields in DefaultShooterCommand final
    - Add JavaDoc
    - Fix typos ("controll" -> "control")
    
    Also remove RobotContainer.getDrive() (it was unused).

--- Robot2024/src/main/java/com/team2813/commands/DefaultShooterCommand.java
+++ Robot2024/src/main/java/com/team2813/commands/DefaultShooterCommand.java
@@ -7,25 +7,45 @@ import com.team2813.subsystems.Shooter;
 
 import edu.wpi.first.wpilibj2.command.Command;
 
-public class DefaultShooterCommand extends Command {
-	private Shooter shooter;
-	private DoubleSupplier controll;
-	public DefaultShooterCommand(Shooter shooter, DoubleSupplier controll) {
+/**
+ * The default command for the {@link Shooter} subsystem.
+ */
+public final class DefaultShooterCommand extends Command {
+	private final Shooter shooter;
+	private final DoubleSupplier control;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param shooter The shooter subsystem.
+	 * @param control Provides values from the driver controller;
+	 *                the returned values MUST be less than or equal to 1.
+	 */
+	public DefaultShooterCommand(Shooter shooter, DoubleSupplier control) {
 		this.shooter = shooter;
-		this.controll = controll;
+		this.control = control;
 		addRequirements(shooter);
 	}
+
 	/**
-	 * The number to multiply results from {@link #controll}. Multiplies by {@value #MULTIPLIER}
+	 * The number to multiply results from {@link #control}. Multiplies by {@value #MULTIPLIER}
 	 */
 	private static final double MULTIPLIER = 1;
-	private static final double DEADZONE = 0.01;
+
 	/**
-	 * Sets the motor to the value from the {@link DoubleSupplier} {@link #controll}.
-	 * Multiplies the result by {@value #MULTIPLIER}. The return from {@link #controll} MUST be less than or equal to 1.
+	 * The maximum value that the controler can have when no one is touching it.
+	 *
+	 * <p>The controler does not always snap back to the center when it is released, so this value accounts for that.
 	 */
-	public void excecute() {
-		double val = controll.getAsDouble();
+	private static final double DEADZONE = 0.01;
+
+	/**
+	 * Sets the motor to the value from the {@link DoubleSupplier} {@link #control}.
+	 * Multiplies the result by {@value #MULTIPLIER}. The return from {@link #control} MUST be less than or equal to 1.
+	 */
+	@Override
+	public void execute() {
+		double val = control.getAsDouble();
 		if (Math.abs(val) > DEADZONE) {
 			shooter.set(ControlMode.DUTY_CYCLE, val * MULTIPLIER);
 		} else {
