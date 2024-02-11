2405695c81b4aab68cbe365f9f5270eccd9ba6be
cuttestkittensrule <mangoiscute95@gmail.com>
2024-02-10 20:41:03 -0800

    spool: the command

---
+++ Robot2024/src/main/java/com/team2813/commands/SpoolCommand.java
@@ -0,0 +1,31 @@
+package com.team2813.commands;
+
+import com.team2813.subsystems.Shooter;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class SpoolCommand extends Command {
+	private double speed;
+	private Shooter shooter;
+	private static final double increase = 0.2;
+	private static final double max = 1;
+	public SpoolCommand(Shooter shooter) {
+		this.shooter = shooter;
+		addRequirements(shooter);
+	}
+	@Override
+	public void initialize() {
+		speed = increase;
+	}
+	@Override
+	public void execute() {
+		shooter.run(speed);
+		if (speed < max) {
+			speed += increase;
+		}
+	}
+	@Override
+	public boolean isFinished() {
+		return speed >= max;
+	}
+}
