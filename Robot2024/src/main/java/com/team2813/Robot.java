aae14f994822d12d77fdbdc5106aa64950e68756
Kevin Cooney <kcooney@gmail.com>
2024-03-17 20:35:42 -0700

    Reset the robot when features are changed while the robot is disabled.

--- Robot2024/src/main/java/com/team2813/Robot.java
+++ Robot2024/src/main/java/com/team2813/Robot.java
@@ -4,17 +4,26 @@
 
 package com.team2813;
 
+import com.team2813.lib2813.feature.Features;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
-
   private RobotContainer m_robotContainer;
+  private Object m_featureState;
+
+  private RobotContainer getContainer() {
+    if (m_robotContainer == null) {
+      m_robotContainer = new RobotContainer();
+    }
+    return m_robotContainer;
+  }
 
   @Override
   public void robotInit() {
+    m_featureState = Features.getState();
     m_robotContainer = new RobotContainer();
   }
 
@@ -27,11 +36,25 @@ public class Robot extends TimedRobot {
   public void disabledInit() {}
 
   @Override
+  public void disabledExit() {
+    Object featureState = Features.getState();
+    if (!featureState.equals(m_featureState)) {
+      /* Features changed; reset the robot. This may have adverse
+       * side effects; it's generally not recommend to enable features
+       * via the UI on a competition day.
+       */
+      m_robotContainer = null;
+      CommandScheduler.getInstance().getDefaultButtonLoop().clear();
+      m_featureState = featureState;
+    }
+  }
+
+  @Override
   public void disabledPeriodic() {}
 
   @Override
   public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+    m_autonomousCommand = getContainer().getAutonomousCommand();
 
     if (m_autonomousCommand != null) {
       m_autonomousCommand.schedule();
@@ -42,11 +65,15 @@ public class Robot extends TimedRobot {
   public void autonomousPeriodic() {}
 
   @Override
-  public void teleopInit() {
+  public void autonomousExit() {
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
+      m_autonomousCommand = null;
     }
   }
+
+  @Override
+  public void teleopInit() {}
 
   @Override
   public void teleopPeriodic() {}
